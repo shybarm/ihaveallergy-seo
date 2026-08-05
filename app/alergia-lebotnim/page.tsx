0ae48135f04a06f7bd26995a94f6cd3e39e867b0
shybarm <Diamondscom@gmail.com>
2026-08-05 10:05:21 +0300

    Update clinic phone and remove street address

--- app/alergia-lebotnim/page.tsx
+++ app/alergia-lebotnim/page.tsx
@@ -491,7 +491,7 @@ export default function PeanutAllergyPage() {
                 <a href="https://ihaveallergy.com/book" target="_blank" rel="noopener noreferrer">קביעת תור לאבחון</a>
               </Button>
               <Button asChild variant="outline" size="lg" className="border-white text-white hover:bg-white/10">
-                <a href="tel:+972545808008"><Phone className="w-4 h-4 ml-2" />054-580-8008</a>
+                <a href="tel:+972525916393"><Phone className="w-4 h-4 ml-2" />052-591-6393</a>
               </Button>
             </div>
           </section>
